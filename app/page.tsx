3e9a3e94a2878b84912cd7e2f62a9d64ab8a09cf
Zhang Zeyu <zhangzeyu2001@gmail.com>
2023-10-24 10:45:15 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,119 @@
+'use client';
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { Slide } from '@mui/material'
+
+const NameForm = (props) => {
+
+  const [show, setShow] = useState(true)
+
+  return (
+    <Slide direction="left" in={show} mountOnEnter unmountOnExit>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-8 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Hi, what&apos;s your name?
+        </h1>
+        <form className="flex flex-col items-center w-full max-w-lg mx-auto mt-6" id="nameForm" onSubmit={(e) => {
+          e.preventDefault();
+          props.handleSubmit(e)
+          setShow(false)
+        }}>
+          <label htmlFor="name" className="sr-only">Name</label>
+          <input id="name" required name="name" type="text" placeholder="Your name" className="w-full px-4 py-2 text-base text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" />
+          <button type="submit" className="inline-flex items-center justify-center px-6 py-3 mt-6 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Continue
+          </button>
+        </form>
+      </div>
+    </Slide>
+  )
+}
+
+const Invoice = (props) => {
+
+  const [show, setShow] = useState(true)
+
+  return (
+    <Slide direction="left" in={show} mountOnEnter unmountOnExit>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 text-left">
+        <h1 className="mb-8 text-3xl font-bold tracking-tight leading-none text-gray-900 dark:text-white">
+          FROM: Finance - Hughes Hall
+        </h1>
+        <p className="my-4">
+          Dear <b>{props.name}</b>,
+        </p>
+        <p className="my-4">
+          Thank you for submitting your Financial Undertaking Form along with any supporting evidence as requested.
+        </p>
+        <p className="my-4">
+          We expect all students who are self-funding to settle their first year of fees in full in advance of arriving for registration.
+        </p>
+        <p className="my-4">
+          Attached is our &apos;Fees Request for Pre-Payment&apos;.
+        </p>
+        <p className="my-4">
+          Please make payment of £48,263.00 by and no later than 15th of September 2023. If paying from an International bank this can take several days.
+        </p>
+        <p className="my-4">
+          Please note that we are unable to confirm your membership until we have received your payment in our bank account.
+        </p>
+        <p className="my-4">
+          To be able to study in the UK, you will need a Visa. Please only make payment when you have received notification of your successful Visa application.
+        </p>
+        <p className="my-4">
+          Please ensure you pay all transfer charges payee & payer.
+        </p>
+        <p className="my-4">
+          Payment should be sent by bank transfer to:
+        </p>
+        <div className="bg-gray-100 dark:bg-gray-800 rounded-md p-4">
+          <p className="my-2">
+            <b>Account Name</b> Hughes Hall, University of Cambridge
+          </p>
+          <p className="my-2">
+            <b>Account Number</b> 7777 7360
+          </p>
+          <p className="my-2">
+            <b>Sort Code</b> 30-64-42
+          </p>
+          <p className="my-2">
+            <b>IBAN</b> GB72LOYD30644277777360
+          </p>
+          <p className="my-2">
+            <b>Swift Code</b> LOYDGB21670
+          </p>
+          <p className="my-2">
+            <b>Address</b> Lloyds Bank, 3 Sidney Street, Cambridge, CB2 3HQ
+          </p>
+        </div>
+        <p className="my-4">
+          Please ensure that you include your full name in the reference.
+        </p>
+        <p className="my-4">
+          Regards
+        </p>
+        <p className="my-4">
+          Finance Office
+        </p>
+      </div>
+    </Slide>
+  )
+}
 
 export default function Home() {
+
+  const [name, setName] = useState('')
+
+  const submit = (e) => {
+    const name = document.getElementById('name').value
+    setName(name)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col justify-between py-24 px-8">
+      <section >
+        {name ? <Invoice name={name} /> : <NameForm handleSubmit={submit} />}
+      </section>
     </main>
   )
 }
